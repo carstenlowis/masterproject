0f80d723c4a761ca2780ecc58fe7a0f93daa3974
clowis <carsten.lowis@alumni.fh-aachen.de>
2021-07-08 19:55:09 +0200

    change 8.7.21

--- read_nifti.py
+++ read_nifti.py
@@ -8,11 +8,16 @@ import matplotlib.gridspec as gridspec
 import seaborn as sns
 from scipy.stats import ttest_ind_from_stats
 from scipy.stats import ranksums
+import pylab
 
 #import data
 path1 = 'Z:\MITARBEITER\Lowis\imaging_data_btu'
 path2 = '01_FE1CP014M3\MRI'
-file = 'FE1-014M3_MRI_KM_coreg.ii'
+file = 'FE1-014M3_MRI_KM_coreg.nii'
 ni_data = join(path1, path2, file)
 
 test = sitk.ReadImage(ni_data)
+
+z = 0
+slice = sitk.GetArrayFromImage(test)[z,:,:]
+plt.imshow(slice)

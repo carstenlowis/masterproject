12319b699201f650410ca2d2324508eda9a234f1
carstenlowis <carsten.lowis@alumni.fh-aachen.de>
2021-07-19 12:06:46 +0200

    change 19.07.2021

--- nn-Unet_data_conversion.py
+++ nn-Unet_data_conversion.py
@@ -1,4 +1,5 @@
 from os.path import isfile, join
+import os
 import pandas as pd
 import numpy as np
 from sklearn import metrics
@@ -12,17 +13,35 @@ import glob
 
 #import data
 #win
-#path = 'Z:\MITARBEITER\Lowis\nn-Unet\nnUnet_imagingdata'
+#path = 'Z:\MITARBEITER\Lowis\nn-Unet'
 #mac
-path = '/Volumes/BTU/MITARBEITER/Lowis/nn-Unet/nnUnet_imagingdata'
-files = glob.glob(path+'/*/*.nii')
+path = '/Volumes/BTU/MITARBEITER/Lowis/nn-Unet'
+imaging_path = join(path, 'nnUnet_imagingdata')
+files = glob.glob(imaging_path+'/*/*.nii')
 
 maskfiles = [file for file in files if file[-8:-3] == 'mask.']
 
 patientids = []
 for i in range(len(maskfiles)):
+    patientids.append(maskfiles[i][len(path)+4:-9])
 
+petfiles = []
+for i in range(len(patientids)):
+    if len(patientids[i]) == 9:
+        petfiles.append(maskfiles[i][:-18]+patientids[i]+'_Sum_coreg.nii')
+    else:
+        petfiles.append(maskfiles[i][:-17] + patientids[i] + '_Sum_coreg.nii')
 
+#create directory
+taskname = 'Task001_BrainTumor'
 
-path2 = '_Patiententabelle_Serial_Imaging_BM_anonymized_07072021.xlsx'
- = pd.read_excel(join(path1, path2))
+nnUNetpath = 'nnUNet_raw_data_base/nnUNet_raw_data'
+directory = join(path, nnUNetpath , taskname)
+
+os.makedirs(directory)
+os.makedirs(join(directory, 'imagesTr'))
+os.makedirs(join(directory, 'imagesTs'))
+os.makedirs(join(directory, 'labelsTr'))
+
+#copy data in new directory
+

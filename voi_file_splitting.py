ddc10567b1551e70cc4b9df754b9cbbdcbb5b30a
carstenlowis <19jejscL98>
2021-09-21 18:24:43 +0200

    21.09.2021 update

--- voi_file_splitting.py
+++ voi_file_splitting.py
@@ -1,13 +1,13 @@
 import os
 import glob
 
-path = 'H:/Pmod_test/'
-voi_file = glob.glob(path+'/*.voi')
+path = 'Y:/data/_Temp/PET_DATA_FOR_SEGMENTATION/BIOPSY_STUDY'
+voi_file = glob.glob(path+'/*/*/*.voi')
 
-os.mkdir(path + 'voi/')
+dir = '/seperated_voi'
+os.mkdir(path + dir)
 
 for count in range(len(voi_file)):
-    #converion from .voi to .txt
     #read data
     orig = open(voi_file[count], 'r')
     orig = orig.read()
@@ -22,7 +22,7 @@ for count in range(len(voi_file)):
     bottom = []
     names = []
     for i in range(len(orig)):
-        if '</UID>' in orig[i][-6:]:
+        if 'voi_time_list_index voi_direction' in orig[i]:
             voi_index.append(i)
             names.append(orig[i+1][:orig[i+1].find('#')])
         elif '#END OF VOIS DEFINITION' in orig[i]:
@@ -32,12 +32,23 @@ for count in range(len(voi_file)):
 
     #create new txt files according to the number of VOIs
     for i in range(n):
-        temp = orig[voi_index[i]:voi_index[i+1]-1]
+        temp = orig[voi_index[i]:voi_index[i+1]]
+
+        if '#VOI TIME LIST NUMBER' in temp[-1]:
+            del temp[-1]
+
         new = header + temp + bottom
-        name = voi_file[count][len(path):-4] + '_VOI_' + names[i][:-1] + '.voi'
+        name = os.path.basename(voi_file[count])[:-4] + '_VOI_' + names[i][:-1] + '.voi'\
+
+        if len(temp) > 9:
+            if 'operation' in temp[10]:
+                name = name[:-4] + '_incompatible.voi'
+
         new [19] = names[i] + '# voi_name'
 
-        f = open(path + '/voi/' + name, 'x')
+        f = open(path + dir + '/' + name, 'x')
         f.write('\n'.join(new))
         f.close()
 
+    print(os.path.basename(voi_file[count])[:-4])
+    print('progress: ', count + 1, ' / ', len(voi_file))
